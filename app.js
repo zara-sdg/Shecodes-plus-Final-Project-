7b8726d2b40e0a8460f2a45ab6f2a74de1cc0694
zara-sdg <129341740+zara-sdg@users.noreply.github.com>
2023-04-04 01:10:43 +0430

    changed the api documentation to shecodes weather api

--- app.js
+++ app.js
@@ -30,17 +30,18 @@ function showTemperature(response) {
   let windSpeed = document.querySelector("#wind-speed");
   let feelsLike = document.querySelector("#feels-like");
   let date = document.querySelector("#date");
-  temperature.innerHTML = Math.round(response.data.main.temp);
-  city.innerHTML = response.data.name;
-  description.innerHTML = response.data.weather[0].description;
-  humidity.innerHTML = response.data.main.humidity;
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+  city.innerHTML = response.data.city;
+  description.innerHTML = response.data.condition.description;
+  humidity.innerHTML = response.data.temperature.humidity;
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
-  feelsLike.innerHTML = Math.round(response.data.main.feels_like);
-  date.innerHTML = showDate(response.data.dt * 1000);
+  feelsLike.innerHTML = Math.round(response.data.temperature.feels_like);
+  date.innerHTML = showDate(response.data.time * 1000);
+  console.log(response.data);
 }
 
-let apiKey = "f09d3949047ab6c9e3bcaf79cf61f619";
+let apiKey = "tfed3e5cefb4b66afc87ab20o9c21efc";
 let city = "milan";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showTemperature);

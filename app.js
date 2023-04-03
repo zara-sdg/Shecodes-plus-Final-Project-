a7be7855f1dfa688f666c73b2740dadb402bacfd
zara-sdg <129341740+zara-sdg@users.noreply.github.com>
2023-04-04 00:32:22 +0430

    created show date function

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
+function showDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wedensday",
+    "Thrsday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showTemperature(response) {
   let temperature = document.querySelector("#temperature-value");
   let city = document.querySelector("#city-name");
   let description = document.querySelector("#description");
   let humidity = document.querySelector("#humidity");
   let windSpeed = document.querySelector("#wind-speed");
+  let feelsLike = document.querySelector("#feels-like");
+  let date = document.querySelector("#date");
   temperature.innerHTML = Math.round(response.data.main.temp);
   city.innerHTML = response.data.name;
   description.innerHTML = response.data.weather[0].description;
   humidity.innerHTML = response.data.main.humidity;
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
-  console.log(response.data);
+  feelsLike.innerHTML = Math.round(response.data.main.feels_like);
+  date.innerHTML = showDate(response.data.dt * 1000);
 }
 
 let apiKey = "f09d3949047ab6c9e3bcaf79cf61f619";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=shiraz&appid=${apiKey}&units=metric`;
+let city = "milan";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showTemperature);
